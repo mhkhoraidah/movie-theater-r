73751035e44bda94b66d31edf88c4623abecf9f5
wishes50 <46933653+wishes50@users.noreply.github.com>
2021-12-04 02:58:28 +0300

    Update movie_theater_starter_code.R

--- movie_theater_starter_code.R
+++ movie_theater_starter_code.R
@@ -1,13 +1,13 @@
 # Potential Questions to Answer:
 # 1. Create snacks that the customers can buy and randomize who buys which snack
 
-#Create snacks dataframe to store the sales
+#Create snacks data frame to store the sales
 snacksDF<-data.frame(Branch=factor(),Theater=integer(), Small.Popcorn=integer(), 
-                   Medium.Popcorn=integer(), Large.Popcorn=integer(),
-                   Icecream=integer(),Soft.Drink=integer(), Frozen=integer())
+                     Medium.Popcorn=integer(), Large.Popcorn=integer(),
+                     Icecream=integer(),Soft.Drink=integer(), Frozen=integer())
 snacksDF<-data.frame(Branch=0,Theater=0,Small.Popcorn=0L, 
-                   Medium.Popcorn=0L, Large.Popcorn=0L,
-                   Icecream=0L,Soft.drink=0L, Frozen=0L)
+                     Medium.Popcorn=0L, Large.Popcorn=0L,
+                     Icecream=0L,Soft.drink=0L, Frozen=0L)
 
 # the function takes snacks data frame to store the sales on it, Branch name, theater number
 # and takes number of visitors.It is return the total revenue of the day for a theater.
@@ -41,6 +41,8 @@ snacksF<-function(snacksDF, BranchName, TheaterN, visitorsN){
 # 8. Scheduling movie function
 
 
+
+
 #'''
 # Object: theater
 #          -type
@@ -56,7 +58,8 @@ snacksF<-function(snacksDF, BranchName, TheaterN, visitorsN){
 ticket_cost <- 12
 ticket_cost_child <- 7 
 
-# List 7 of your favorite movies
+# Movie List
+# DATA:         Movie Name  | Age Rating  | Number of Shows each day
 movies <- rbind(c("Limitless", "PG-13", 3), 
                 c("Taken", "PG-13", 1), 
                 c("The Old Guard", "R", 2),
@@ -65,22 +68,35 @@ movies <- rbind(c("Limitless", "PG-13", 3),
                 c("Raya and the Last Dragon", "PG", 4),
                 c("Rush Hour 3", "PG-13", 3))
 
-show_time = c("13:30", "18:00", "22:40")
+# Theater types based on capacity and ticket cost
+# DATA:         Type Name  | Seats  | Adult Cost  | Child Cost
+theatersType <- rbind(c("VIP",      60,  20, 15),
+                      c("Standard", 120, 20, 15),
+                      c("MAX",      180, 20, 15))
 
-theaters <-  6 # How many screens does the theater have? (assume 1 per movie)
+# Branch and size of the each branch based on the number of screens have
+#  DATA:       Branch Location  | VIP | Standard | MAX
+branch <-  rbind(c("Riyadh", 2, 5, 2), 
+                 c("Dammam", 1, 3, 1), 
+                 c("Jeddah", 1, 4, 1))
+
+
 seats <-  120 # How many seats does each theater hold
 week_days <- rep(0, 7)  # Store totals for each day
-  
-    # iterate through the week
+
+
+runTheaaters <- function(week_days, theaters) {
+  # iterate through the week
     for (day_num in 1:length(week_days)) {
       # Keep track of total revenue for the day
+      if (week_days[day_num]== "Saturday" | week_days[day_num]=="Sunday"){
       daily_revenue <- 0
       
       # iterate through the amount of screens on a particular day
       for (theater_number in 1:theaters) {
         
         # Calculate  how many adults and children are watching the movie
-        visitors_adults <- sample(1:seats, 1)
+        visitors_adults <- sample(60:seats, 1)
         avalibale_seats <- seats - visitors_adults
         visitors_children <- 0
         
@@ -94,7 +110,7 @@ week_days <- rep(0, 7)  # Store totals for each day
         if(movies[theater_number,2] != "PG" && movies[theater_number,2] != "PG-13") {
           cat("**children are not allowed because movie is ", movies[theater_number,2], "**\n")
         }
-
+        
         
         # Calculate the revenue for adults and children
         movie_revenue <- (visitors_adults * ticket_cost) + (visitors_children * ticket_cost_child)
@@ -111,14 +127,61 @@ week_days <- rep(0, 7)  # Store totals for each day
       cat("Daliy total revenue: $", week_days[day_num], "\n")
       cat("------------------------------------------\n")
       
-    }
-    
-    # Make a barchart showing total revenue per day
-    day_name <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    barplot(week_days, names.arg = day_name, horiz = FALSE)
+      }
+      else{
+        for (day_num in 1:week_days[5]) {
+          # Keep track of total revenue for the day
+          daily_revenue <- 0
+          
+          # iterate through the amount of screens on a particular day
+          for (theater_number in 1:theaters) {
+            
+            # Calculate  how many adults and children are watching the movie
+            visitors_adults <- sample(1:seats, 1)
+            avalibale_seats <- seats - visitors_adults
+            visitors_children <- 0
+            
+            # conditional statements for movies that may be PG-13 and children are not allowed to watch
+            if(movies[theater_number,2] == "PG" || movies[theater_number,2] == "PG-13") {
+              visitors_children <- sample(1:avalibale_seats,1) # this is should be the the rest of available seats or less
+              avalibale_seats <- avalibale_seats - visitors_children
+            } 
+            cat("\nThe theater #", theater_number, "\n\tMovie : ", movies[theater_number], "(", movies[theater_number, 2] , ")", "\n\tAduilt booked: ", visitors_adults,
+                "\n\tChildren booked: ", visitors_children, "\n\tAbalibale seats: ", avalibale_seats, "/", seats ,"\n")
+            if(movies[theater_number,2] != "PG" && movies[theater_number,2] != "PG-13") {
+              cat("**children are not allowed because movie is ", movies[theater_number,2], "**\n")
+            }
+            
+            
+            # Calculate the revenue for adults and children
+            movie_revenue <- (visitors_adults * ticket_cost) + (visitors_children * ticket_cost_child)
+            
+            # Calculate revenue, and add to running total for the day
+            week_days[day_num] <- week_days[day_num] + movie_revenue
+            
+            # ------> Need to add Snacks revenue
+            # Calculate snacks revenue
+            
+          }
+          # Save total to the corresponding day
+          cat("------------------------------------------\n")
+          cat("Daliy total revenue: $", week_days[day_num], "\n")
+          cat("------------------------------------------\n")
+          
+        }
+      }
+  }
+}
 
-    # Make any other chart
-    pie(week_days, label = day_name, main = "Revenue")
-    
-    # Which day had the highest revenue? 
-    cat("The highest revenue is $", max(week_days), "in", day_name[which(week_days == max(week_days))])
+
+
+# Make a barchart showing total revenue per day
+day_name <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+barplot(week_days, names.arg = day_name, horiz = FALSE)
+
+# Make any other chart
+pie(week_days, label = day_name, main = "Revenue")
+
+# Which day had the highest revenue? 
+cat("The highest revenue is $", max(week_days), "in", day_name[which(week_days == max(week_days))])
+
