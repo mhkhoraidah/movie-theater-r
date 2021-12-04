5bc78dccdab87c5e5c5de1004d38b13e752e2d20
Hala Alsouly <u5995105@anu.edu.au>
2021-12-05 02:22:35 +1100

    Revenues are solved, theater function work done, print report

--- movie_theater_starter_code.R
+++ movie_theater_starter_code.R
@@ -1,14 +1,6 @@
 
-################### Libraries ###############
-#install.packages(RCurl)
-#library(RCurl)
 
-
-# URL<-'https://www.dropbox.com/s/wlpn9lf5dz1d7dj/Theaters.csv?dl=0'
-# url_content <- getURL(URL)
-# Theaters<-read.csv('https://www.dropbox.com/s/wlpn9lf5dz1d7dj/Theaters.csv?dl=0',...)
-
-Theaters<-read.csv('Data governance bootcamp\\R\\Week 6\\day5\\movie-theater-r\\Theaters.csv')
+#Theaters<-read.csv('Data governance bootcamp\\R\\Week 6\\day5\\movie-theater-r\\Theaters.csv',stringsAsFactors = FALSE)
 #print(Theaters)
 
 
@@ -46,112 +38,12 @@ snacksF<-function(snacksDF, BranchName, TheaterN, visitorsN){
   snacksDF<<-snacksDF
   return (snacksSales)
 }
-#snacksF(snacksDF,'R',1,100)
-#snacksF(snacksDF,'S',1,200)
-#print(snacksDF)
+
 
 ###############################################################################################
 
-# 2. Pretend you own multiple theaters and run two simulations to represent each theater and plot the results
-# 3. Create conditional statements for movies that may be PG-13 and children are not allowed to watch
-# 4. Discount Student
-# 5. Create function
-# 6. theaters types (VIP, St)
-# 7. Random number of seats will start form seats/2:seats
-# 8. Scheduling movie function
-
-
-#'''
-# Object: theater
-#          -type
-#          -seats
-#          -adult_cost
-#          -child_cost
-#'''
-
-
-##################################### Theaters Function ######################################
-runTheaters<-function(theatersDF, moviesDF){
-
-  #Get branches names
-  uniqueBranch <- unique(theatersDF[c('Branch')])
-  uniqueBranch<-uniqueBranch[,1]
-  branchesRevenue<-data.frame(uniqueBranch,stringsAsFactors = TRUE)
-  
-  #create revenues dataset
-  for (i in 1:7){
-    d <- rep(0, length(uniqueBranch))  
-    branchesRevenue<- cbind(branchesRevenue,d)
-  }
-  
-  print((branchesRevenue))
-  #iterate through theaters
-  for (i in 1:nrow(theatersDF)){
-
-    # iterate through the week
-    for (day_num in 1:length(week_days)) {
-      # Keep track of total revenue for the day
-      daily_revenue <- 0
-      
-      #get theater info
-      seats<-as.numeric(theatersDF[i, ]['seats'])
-      branchName<-as.character(theatersDF[i, ]['Branch'])
-      theaterN<-as.numeric(theatersDF[i, ]['Theater'])
-      AdultCost<-as.numeric(theatersDF[i, ]['Adult.Cost'])
-      ChildCost<-as.numeric(theatersDF[i, ]['Child.Cost'])
-      
-        # Calculate  how many adults and children are watching the movie
-        visitors_adults <- sample(1:seats, 1)
-        avalibale_seats <- seats - visitors_adults
-        visitors_children <- 0
-        
-        # conditional statements for movies that may be PG-13 and children are not allowed to watch
-        if(movies[theater_number,2] == "PG" || movies[theater_number,2] == "PG-13") {
-          visitors_children <- sample(1:avalibale_seats,1) # this is should be the the rest of available seats or less
-          avalibale_seats <- avalibale_seats - visitors_children
-        } 
-        # cat("\nThe theater #", theater_number, "\n\tMovie : ", movies[theater_number], "(", movies[theater_number, 2] , ")", "\n\tAduilt booked: ", visitors_adults,
-        #     "\n\tChildren booked: ", visitors_children, "\n\tAbalibale seats: ", avalibale_seats, "/", seats ,"\n")
-        # if(movies[theater_number,2] != "PG" && movies[theater_number,2] != "PG-13") {
-        #   cat("**children are not allowed because movie is ", movies[theater_number,2], "**\n")
-        # }
-        
-        
-        # Calculate the revenue for adults and children
-        movie_revenue <- (visitors_adults * AdultCost) + (visitors_children * ChildCost)
-        
-        # Calculate snacks revenue
-        snakcsRevenue<-snacksF(snacksDF, branchName, theaterN, seats)
-        
-        # Calculate revenue, and add to running total for the day
-        x<-as.numeric(which(branchesRevenue$uniqueBranch == branchName))
-        print(x)
-        branchesRevenue[x,day_num+1] <- 
-          branchesRevenue[x ,day_num+1] 
-        + movie_revenue + snakcsRevenue
-        branchesRevenue[1,3]<-branchesRevenue[1,3]+40
-        #print(branchesRevenue[uniqueBranch== branchName,day_num+1])
-        # ------> Need to add Snacks revenue
-        # Calculate snacks revenue
-        
-      
-      # # Save total to the corresponding day
-      # cat("------------------------------------------\n")
-      # cat("Daliy total revenue: $", week_days[day_num], "\n")
-      # cat("------------------------------------------\n")
-      
-    }
-    
-  }
-  
-  print((branchesRevenue))
-}
-
-# Cost for adults and children
-#ticket_cost <- 12
-#ticket_cost_child <- 7 
-
-# List 7 of your favorite movies
+# Movie List
+# DATA:         Movie Name  | Age Rating  | Number of Shows each day
 movies <- rbind(c("Limitless", "PG-13", 3), 
                 c("Taken", "PG-13", 1), 
                 c("The Old Guard", "R", 2),
@@ -160,20 +52,175 @@ movies <- rbind(c("Limitless", "PG-13", 3),
                 c("Raya and the Last Dragon", "PG", 4),
                 c("Rush Hour 3", "PG-13", 3))
 
+# Branchs and size of the each branch based on the number of screens have
+#  DATA:       Branch Location  | VIP | Standard | MAX
+branchs <-  rbind(c("Riyadh", 2, 5, 2), 
+                  c("Dammam", 1, 3, 1), 
+                  c("Jeddah", 1, 4, 1))
+# Theater Types based on capacity and ticket cost
+# DATA:         Type Name  | Seats  | Adult Cost  | Child Cost
+theatersType <- rbind(c("VIP",      60,  40, 30),
+                      c("Standard", 120, 20, 15),
+                      c("MAX",      180, 25, 20))
+
+# This function generate a theaters data set for multiple branch by passing branchs and theatersTypes
+generateDataSet <- function(branchs, theatersType){
+  # Initialize the dataset columns to empty state
+  branch_col <- NULL
+  theater_num_col <- NULL
+  type_col <- NULL
+  seats_col <- NULL
+  adult_cost_col <- NULL
+  child_cost_col <- NULL
+  movie_name_col <- NULL
+  movie_rating_age_col <- NULL
+  show_number_col <- NULL
+  
+  
+  # Loop over branchs and theatersType to create dataset
+  for(row in 1:nrow(branchs)) {
+    
+    # Count theater number for each branch in total including all types of theater (VIP, Standard, Max)
+    countTheater <- 0
+    
+    for(col in 2:ncol(branchs)) {
+      # total screen for each theater type and covert the string digit to integer number
+      screensCount <- as.numeric(branchs[row, col])
+      # calculate the total of all screens for each branch
+      countTheater <- countTheater + screensCount
+      
+      # Loading the data for each column
+      branch_col     <- c(branch_col, rep(branchs[row], screensCount))
+      type_col       <- c(type_col, rep(theatersType[col-1], screensCount))
+      seats_col      <- c(seats_col, rep(theatersType[col+2], screensCount))
+      adult_cost_col <- c(adult_cost_col, rep(theatersType[col+5], screensCount))
+      child_cost_col <- c(child_cost_col, rep(theatersType[col+8], screensCount))
+    }
+    
+    # Create random movies assign for each theater
+    for(movie in 1:countTheater) {
+      # Get a random movie form Movies List in the top
+      random_movie_index <- sample(1:nrow(movies),1)
+      
+      # Loading and assign random movies to each theater
+      movie_name_col <- c(movie_name_col, movies[random_movie_index,1])
+      movie_rating_age_col <- c(movie_rating_age_col, movies[random_movie_index,2])
+      show_number_col <- c(show_number_col, sample(1:4, 1))
+    }
+    
+    # List of theaters number for all branch
+    theater_num_col <- c(theater_num_col, c(1:countTheater))
+  }
+  
+  # create the data frame 
+  dataset = data.frame(Branch         = branch_col,
+                       Theater.Number = theater_num_col,
+                       Type           = type_col,
+                       Seats          = seats_col,
+                       Adult.Cost     = adult_cost_col,
+                       Child.Cost     = child_cost_col,
+                       Movie_Name      = movie_name_col,
+                       Age.Rating = movie_rating_age_col,
+                       Show_Number      = show_number_col,
+                       stringsAsFactors = FALSE)
+  
+  return(dataset)
+}
+
+# call generateDataSet function
+Theaters<-generateDataSet(branchs, theatersType)
+#View(Theaters)
+
+##################################### Theaters Function ######################################
+# It takes theater dataframe and returns the week revenues for each branch as a dataframe
+runTheaters<-function(theatersDF, moviesDF){
+
+  #Get branches names from the dataset
+  uniqueBranch <- unique(theatersDF[c('Branch')])
+  uniqueBranch<-uniqueBranch[,1]
+  branchesRevenue<-data.frame(uniqueBranch,stringsAsFactors = FALSE)
+  
+  #create revenues dataset, 7 days for each branch
+  for (i in 1:7){
+    d <- rep(0, length(uniqueBranch))
+    branchesRevenue<- cbind(branchesRevenue,d)
+  }
+  
+  #iterate through theaters
+  for (i in 1:nrow(theatersDF)){
+
+    # iterate through the week
+    for (day_num in 1:7) {
+      # Keep track of total revenue for the day
+      daily_revenue <- 0
+      
+      #get theater info
+      seats<- as.numeric(theatersDF[i, ]['Seats'])
+      branchName<- as.character(theatersDF[i, ]['Branch'])
+      theaterN<- as.numeric(theatersDF[i, ]['Theater.Number'])
+      AdultCost<- as.numeric(theatersDF[i, ]['Adult.Cost'])
+      ChildCost<- as.numeric(theatersDF[i, ]['Child.Cost'])
+      AgeRating<- as.character(theatersDF[i,]['Age.Rating'])
+      
+      #The sample starts by 1 or half of the space if it is weekend
+      startSeat=1L
+      if (day_num %in% (5:7)){
+        startSeat= seats %/% 2
+      }
+      # Calculate  how many adults and children are watching the movie
+      visitors_adults <- sample(startSeat:seats, 1)
+      avalibale_seats <- seats - visitors_adults
+      visitors_children <- 0
+      
+      # conditional statements for movies that may be PG-13 and children are not allowed to watch
+      if( AgeRating == "PG" || AgeRating == "PG-13") {
+        visitors_children <- sample(1:avalibale_seats,1) # this is should be the the rest of available seats or less
+        avalibale_seats <- avalibale_seats - visitors_children
+      } 
+
+      # Calculate the revenue for adults and children
+      movie_revenue <- (visitors_adults * AdultCost) + (visitors_children * ChildCost)
+      
+      # Calculate snacks revenue
+      snakcsRevenue<-snacksF(snacksDF, branchName, theaterN, seats-avalibale_seats)
+      
+      # Calculate the total revenue of the day, and add it to the revenues datafram by matching
+      # the branch name and the day index
+      x<-which(uniqueBranch == branchName )
+      branchesRevenue[x,day_num+1] <- branchesRevenue[x ,day_num+1]  + movie_revenue + snakcsRevenue
+      
+    }
+    
+  }
+  #Rename the columns to correct names
+  names(branchesRevenue)[2]<-"Sunday"
+  names(branchesRevenue)[3]<-"Monday"
+  names(branchesRevenue)[4]<-"Tuesday"
+  names(branchesRevenue)[5]<-"Wednesday"
+  names(branchesRevenue)[6]<-"Thursday"
+  names(branchesRevenue)[7]<-"Friday"
+  names(branchesRevenue)[8]<-"Saturday"
+  
+  cat("---------------------------------------------------------------------\n")
+  cat("\t The Total revenues For The Theaters", "\n")
+  print((branchesRevenue))
+  cat("---------------------------------------------------------------------\n")
+  return (branchesRevenue)
+}
+
+printMaxRevenue<-function(RevenuesDF){
+  # Transpose the dataset
+  newDF=t(RevenuesDF)
+
+  for (i in 1: ncol(newDF)){
+    cat("------------------------------------------\n")
+    cat("Maximum total revenue of the week for", newDF[1,i],": $", max(newDF[2:nrow(newDF),i]), "\n")
+    cat("------------------------------------------\n")
+  }
+  
+}
 show_time = c("13:30", "18:00", "22:40")
 
-runTheaters(Theaters, movies)
+data<-runTheaters(Theaters, movies)
+printMaxRevenue(data)
 
-theaters <-  6 # How many screens does the theater have? (assume 1 per movie)
-seats <-  120 # How many seats does each theater hold
-
-    
-    # Make a barchart showing total revenue per day
-    day_name <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
-    barplot(week_days, names.arg = day_name, horiz = FALSE)
-
-    # Make any other chart
-    pie(week_days, label = day_name, main = "Revenue")
-    
-    # Which day had the highest revenue? 
-    cat("The highest revenue is $", max(week_days), "in", day_name[which(week_days == max(week_days))])
